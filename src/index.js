645cb756f6fa51a4944136b2acd4c6999b5d3718
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-08-06 15:15:14 -0400

    added axios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // Imports at the top of the file!
 // We never nest imports inside blocks of code!
-
+import axios from 'axios'
 
 // 👉 TASK 1- Test out the following endpoints:
+
+
 
 //     https://lambda-times-api.herokuapp.com/friends
 //     https://lambda-times-api.herokuapp.com/friends/1
@@ -16,10 +18,32 @@
 //    * With Chrome
 //    * With JS using the native fetch
 
+// function handleSucess(something){
+//   debugger
+// }
+
+// fetch('https://lambda-times-api.herokuapp.com/friends')
+//   .then(handleSucess)
+  
+
+console.log('about to request')
+fetch('https://lambda-times-api.herokuapp.com/friends') // returns a promise, but not the data that we want //dont need to set it to a variable
+  .then(response =>{
+    console.log('about to parse the response body')
+    return response.json() // returns another promise (same as fetch itself)
+  })
+  .then(parsedResponse => {
+    console.log('the parsed body is here')
+    console.log(parsedResponse)
+  })
+  .catch(error => { //if soemthing fail catastrophically, this callback will be invoked by chrome
+    debugger // leave a breakpoint at catch at all time during developement
+  })
+console.log('finished requesting')
 
 // 👉 TASK 2- Select the "entry point", the element
 // inside of which we'll inject our dog cards 
-const entryPoint = null
+const entryPoint = document.querySelector('.entry')
 
 
 // 👉 TASK 3- `dogCardMaker` takes an object and returns a Dog Card.
@@ -50,12 +74,27 @@ function dogCardMaker({ imageURL, breed }) {
 // 👉 TASK 4- Bring the Axios library into the project using one of two methods:
 //    * Traditional way: put another script tag inside index.html (`https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js`)
 //    * Proyects with npm: install it with npm and import it into this file
-
+// console.log(axios)
+// axios.get('https://lambda-times-api.herokuapp.com/breeds')
+//   .then(stuff =>{
+//     debugger
+//   })
+//   .catch(err =>{
+//     debugger
+//   })
 
 // 👉 TASK 5- Fetch dogs from `https://dog.ceo/api/breed/{breed}/images/random/{number}`
 //    * ON SUCCESS: use the data to create dogCards and append them to the entry point
 //    * ON FAILURE: log the error to the console
 //    * IN ANY CASE: log "done" to the console
+
+// test the enpoint first
+axios.get('https://dog.ceo/api/breed/mastiff/images/random/5')
+  .then(response => { // in the case of axios, the future data is an object with a 'data' prop (among others)
+  })
+  .catch(errror =>{ //404 will fall here with axios
+    debugger
+  })
 
 
 // 👉 (OPTIONAL) TASK 6- Wrap the fetching operation inside a function `getDogs`
